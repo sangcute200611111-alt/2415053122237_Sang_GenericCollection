69e284a5edefbd816b11365b0f98eb12a7f230c3
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-17 21:51:01 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2415053122237_Sang_GenericCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
