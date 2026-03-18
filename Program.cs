0346e7696ab908838899b7b13b06654e60ebe221
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:42:56 +0700

    Bổ sung chú thích giải thuật đếm tần suất ký tự bằng Dictionary

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Thiết lập font chữ tiếng Việt cho nhập và xuất dữ liệu
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
 
@@ -17,16 +18,21 @@ namespace BT_GenericCollection
             Console.Write("Nhập chuỗi: ");
             string input = Console.ReadLine();
 
+            // Sử dụng Dictionary để lưu trữ cặp: Ký tự (Key) - Số lần (Value)
             Dictionary<char, int> counts = new Dictionary<char, int>();
 
             foreach (char c in input)
             {
+                // Bỏ qua ký tự khoảng trắng
                 if (c == ' ') continue;
+
+                // Nếu ký tự đã tồn tại thì tăng số lượng, ngược lại thì thêm mới
                 if (counts.ContainsKey(c)) counts[c]++;
                 else counts[c] = 1;
             }
 
             Console.WriteLine("\nKết quả:");
+            // Duyệt Dictionary để in tần suất xuất hiện của từng ký tự
             foreach (var item in counts)
             {
                 Console.WriteLine($"- '{item.Key}': {item.Value}");
