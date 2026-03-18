93c7121a9674018ded6506192c57a8f0ac3b09df
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 19:01:44 +0700

    Hoàn thành bài 15

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_GenericCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string info = "2415053122237 - Võ Văn Sang";
+            Console.WriteLine($"{info} | Bài 15");
+
+            Dictionary<string, string> tuDien = new Dictionary<string, string> {
+                {"hello", "xin chào"},
+                {"student", "sinh viên"},
+                {"computer", "máy tính"}
+            };
+
+            Console.WriteLine("Từ điển hiện có: hello, student, computer");
+            Console.Write("Nhập từ tiếng Anh cần tra: ");
+            string key = Console.ReadLine().ToLower();
+
+            if (tuDien.ContainsKey(key))
+                Console.WriteLine($"=> Nghĩa của từ '{key}' là: {tuDien[key]}");
+            else
+                Console.WriteLine("=> Rất tiếc, từ này chưa có trong từ điển.");
+
+            Console.ReadKey();
+        }
+    }
+}
