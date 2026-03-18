042f1d130fb3e3a9fee0e42450810c398ab892dd
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 17:39:27 +0700

    Hoàn thành bài 3: Tìm số lớn nhất

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _2415053122237_Sang_GenericCollection
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_GenericCollection
 {
-    internal class Program
+    public class Student { public int Id; public string Name; public double Score; }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            string info = "2415053122237 - Võ Văn Sang";
+            Console.WriteLine($"{info} | Bài 3");
+            List<int> list = new List<int>();
+            Console.Write("Nhập số lượng: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Số {i + 1}: ");
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Số lớn nhất: " + list.Max());
+            Console.ReadKey();
         }
     }
 }
