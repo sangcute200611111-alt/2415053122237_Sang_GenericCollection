29496ced7c8e362910bbe6855603f55fac9eaabd
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:18:57 +0700

    Bổ sung chú thích quy trình nhập liệu và duyệt Generic List để tính tổng

--- Program.cs
+++ Program.cs
@@ -10,17 +10,24 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Cài đặt hiển thị tiếng Việt và thông tin sinh viên
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 2");
+
+            // Khởi tạo List số nguyên và nhập số lượng phần tử
             List<int> list = new List<int>();
             Console.Write("Nhập số lượng phần tử: ");
             int n = int.Parse(Console.ReadLine());
+
+            // Vòng lặp nhập từng giá trị và thêm vào danh sách
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Nhập số {i + 1}: ");
                 list.Add(int.Parse(Console.ReadLine()));
             }
+
+            // Sử dụng phương thức Sum() của LINQ để tính tổng nhanh
             Console.WriteLine("Tổng các phần tử: " + list.Sum());
             Console.ReadKey();
         }
