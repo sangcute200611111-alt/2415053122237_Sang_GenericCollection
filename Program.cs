af412152cc70e0e47cf00024313bf4abd1777ce3
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:37:08 +0700

    Bổ sung chú thích về cách sử dụng phương thức Reverse trong List

--- Program.cs
+++ Program.cs
@@ -8,13 +8,21 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Cài đặt hiển thị tiếng Việt và thông tin sinh viên
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 8");
+
+            // Khởi tạo danh sách Generic List với các giá trị mẫu
             List<int> list = new List<int> { 1, 2, 3, 4, 5 };
             Console.WriteLine("Danh sách gốc: " + string.Join(", ", list));
+
+            // Sử dụng phương thức Reverse để đảo ngược thứ tự các phần tử
             list.Reverse();
+
+            // In danh sách sau khi đã đảo ngược ra màn hình
             Console.WriteLine("Danh sách đảo ngược: " + string.Join(", ", list));
+
             Console.ReadKey();
         }
     }
