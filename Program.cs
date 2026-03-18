77865c4daa86a4ba5f3edc97a108fd9326284077
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 18:37:43 +0700

    Hoàn thành bài 12

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_GenericCollection
+{
+    public class Student { public int Id; public string Name; }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string info = "2415053122237 - Võ Văn Sang";
+            Console.WriteLine($"{info} | Bài 12");
+            List<Student> ds = new List<Student> {
+                new Student { Id = 1, Name = "Sang" },
+                new Student { Id = 2, Name = "An" }
+            };
+            Console.Write("Nhập tên sinh viên cần tìm: ");
+            string search = Console.ReadLine();
+            var sv = ds.Find(s => s.Name.Equals(search, StringComparison.OrdinalIgnoreCase));
+            if (sv != null) Console.WriteLine($"Tìm thấy sinh viên ID: {sv.Id}");
+            else Console.WriteLine("Không tìm thấy sinh viên này.");
+            Console.ReadKey();
+        }
+    }
+}
