4894a85bd60144ccc532d5c5691666ae22b0a64a
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:38:43 +0700

    Bổ sung chú thích quy trình nhập liệu và lọc phần tử trùng lặp bằng Distinct

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // Cần Linq để dùng Distinct()
 using System.Text;
 
 namespace BT_GenericCollection
@@ -9,14 +9,17 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Thiết lập hiển thị tiếng Việt và thông tin sinh viên
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 9");
 
+            // Khởi tạo danh sách Generic List kiểu int
             List<int> list = new List<int>();
             Console.Write("Nhập số lượng phần tử: ");
             int n = int.Parse(Console.ReadLine());
 
+            // Vòng lặp nhập dữ liệu từ bàn phím vào danh sách
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Nhập phần tử thứ {i + 1}: ");
@@ -25,6 +28,7 @@ namespace BT_GenericCollection
 
             Console.WriteLine("\nDanh sách vừa nhập: " + string.Join(", ", list));
 
+            // Sử dụng Distinct() để lọc ra các phần tử duy nhất (không trùng lặp)
             var uniqueList = list.Distinct().ToList();
 
             Console.WriteLine("Danh sách sau khi loại bỏ phần tử trùng:");
