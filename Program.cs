04451a589d0a3530088ca355a8f247462ea023b9
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:26:24 +0700

    Bổ sung chú thích giải thuật tìm giá trị lớn nhất trong Generic List

--- Program.cs
+++ Program.cs
@@ -10,18 +10,27 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Cấu hình hiển thị tiếng Việt
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 3");
+
+            // Khởi tạo danh sách số nguyên
             List<int> list = new List<int>();
+
             Console.Write("Nhập số lượng: ");
             int n = int.Parse(Console.ReadLine());
+
+            // Vòng lặp nhập dữ liệu
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Số {i + 1}: ");
                 list.Add(int.Parse(Console.ReadLine()));
             }
+
+            // In ra giá trị lớn nhất trong danh sách
             Console.WriteLine("Số lớn nhất: " + list.Max());
+
             Console.ReadKey();
         }
     }
