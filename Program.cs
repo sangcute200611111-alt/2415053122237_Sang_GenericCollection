c8a10be6fd5bc0ce9304feaa925160870da5f2a9
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 18:43:02 +0700

    Hoàn thành bài 13

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_GenericCollection
+{
+    public class Student { public int Id; public string Name; }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string info = "2415053122237 - Võ Văn Sang";
+            Console.WriteLine($"{info} | Bài 13");
+            List<Student> ds = new List<Student> {
+                new Student { Id = 1, Name = "Sang" },
+                new Student { Id = 2, Name = "An" }
+            };
+            Console.WriteLine("Số lượng sinh viên trong danh sách: " + ds.Count);
+            Console.ReadKey();
+        }
+    }
+}
