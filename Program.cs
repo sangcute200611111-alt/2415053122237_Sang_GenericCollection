e419a4e0cdf3bd6cc7036083f1f2b7f349f5e469
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 17:29:21 +0700

    Hoàn thành bài 1: Tạo List số nguyên

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace _2415053122237_Sang_GenericCollection
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_GenericCollection
 {
-    internal class Program
+    public class Student { public int Id; public string Name; public double Score; }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            string info = "2415053122237 - Võ Văn Sang";
+            Console.WriteLine($"{info} | Bài 1");
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Nhập số {i + 1}: ");
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Danh sách: " + string.Join(", ", list));
+            Console.ReadKey();
         }
     }
 }
