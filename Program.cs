5dd0c9f2d13292f00a8f8ab0334af7a37f487168
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:29:57 +0700

    Hoàn thiện phần chú thích và trình bày code bài 4

--- Program.cs
+++ Program.cs
@@ -8,26 +8,25 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
-            // Cài đặt hiển thị tiếng Việt và thông tin sinh viên
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 4");
 
-            // Khởi tạo danh sách Generic List để lưu các số nguyên
             List<int> list = new List<int>();
 
             Console.Write("Nhập số lượng số cần kiểm tra: ");
             int n = int.Parse(Console.ReadLine());
 
-            // Vòng lặp for để nhập dữ liệu từ bàn phím vào List
+            // Thực hiện nhập dữ liệu thông qua vòng lặp
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Nhập số {i + 1}: ");
                 list.Add(int.Parse(Console.ReadLine()));
             }
 
-            // Đếm số lượng các số chẵn trong danh sách và in kết quả
-            Console.WriteLine("Số lượng số chẵn: " + list.Count(x => x % 2 == 0));
+            // Hiển thị kết quả đếm số chẵn ra màn hình Console
+            int soChan = list.Count(x => x % 2 == 0);
+            Console.WriteLine("Số lượng số chẵn: " + soChan);
 
             Console.ReadKey();
         }
