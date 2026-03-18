997ac3de250dd4df45db7d03d1e56670c5ed9de0
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:13:32 +0700

    Thêm chú thích giải thích phương thức tìm giá trị nhỏ nhất

--- Program.cs
+++ Program.cs
@@ -9,12 +9,20 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
+            // Thiết lập hiển thị tiếng Việt
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 10");
+
+            // Khởi tạo danh sách số nguyên có sẵn phần tử
             List<int> list = new List<int> { 15, 42, 5, 89, 23 };
+
+            // In danh sách ra màn hình Console
             Console.WriteLine("Danh sách: " + string.Join(", ", list));
+
+            // Sử dụng phương thức Min() để tìm giá trị nhỏ nhất
             Console.WriteLine("Giá trị nhỏ nhất: " + list.Min());
+
             Console.ReadKey();
         }
     }
