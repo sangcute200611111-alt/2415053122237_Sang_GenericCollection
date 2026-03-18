d36ead2b34b5ae6f3e913121b8a814b711ba1768
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-18 20:39:50 +0700

    Hoàn thiện phần chú thích và trình bày code bài 9

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq; // Cần Linq để dùng Distinct()
+using System.Linq;
 using System.Text;
 
 namespace BT_GenericCollection
@@ -9,30 +9,32 @@ namespace BT_GenericCollection
     {
         static void Main(string[] args)
         {
-            // Thiết lập hiển thị tiếng Việt và thông tin sinh viên
             Console.OutputEncoding = Encoding.UTF8;
             string info = "2415053122237 - Võ Văn Sang";
             Console.WriteLine($"{info} | Bài 9");
 
-            // Khởi tạo danh sách Generic List kiểu int
             List<int> list = new List<int>();
             Console.Write("Nhập số lượng phần tử: ");
-            int n = int.Parse(Console.ReadLine());
+            int sl = int.Parse(Console.ReadLine());
 
-            // Vòng lặp nhập dữ liệu từ bàn phím vào danh sách
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < sl; i++)
             {
                 Console.Write($"Nhập phần tử thứ {i + 1}: ");
-                list.Add(int.Parse(Console.ReadLine()));
+                int giaTri = int.Parse(Console.ReadLine());
+                list.Add(giaTri);
             }
 
-            Console.WriteLine("\nDanh sách vừa nhập: " + string.Join(", ", list));
+            // Hiển thị danh sách gốc đã nhập
+            string dsGoc = string.Join(", ", list);
+            Console.WriteLine("\nDanh sách vừa nhập: " + dsGoc);
 
-            // Sử dụng Distinct() để lọc ra các phần tử duy nhất (không trùng lặp)
+            // Xử lý loại bỏ các phần tử trùng nhau trong danh sách
             var uniqueList = list.Distinct().ToList();
 
+            // Hiển thị danh sách kết quả sau khi đã lọc
+            string dsLoc = string.Join(", ", uniqueList);
             Console.WriteLine("Danh sách sau khi loại bỏ phần tử trùng:");
-            Console.WriteLine(string.Join(", ", uniqueList));
+            Console.WriteLine(dsLoc);
 
             Console.WriteLine("\nNhấn phím bất kỳ để thoát...");
             Console.ReadKey();
